4f9cc830cd91125e22a48f0203bfb7f44b17e867
Isaac Svedin <swedinisaac@gmail.com>
2025-04-22 09:14:28 +0200

    Added the start page

--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })

e6bab27b565cda0b6811e8c05cd18fa014e7bed6
Isaac Svedin <swedinisaac@gmail.com>
2025-04-22 07:50:06 +0200

    Set things up and added a menu component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './pages/start/start.component';
+import { ConvertComponent } from './pages/convert/convert.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "start", component: StartComponent },
+    { path: "convert", component: ConvertComponent },
+    { path: "about", component: AboutComponent },
+    { path: "", redirectTo: "/start", pathMatch: "full"}
+];
